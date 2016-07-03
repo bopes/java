a0eb34b34ebde28565a805e53e3318e832ab06ec
bopes <clcblee@gmail.com>
2016-07-03 15:25:33 -0700

    Rename TutorialClass to Tutorial

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -1,8 +1,9 @@
 // This is a tutorial
-public class TutorialClass {
+public class Tutorial {
 	
 	public static void main(String[] args){
 //		this is our main void
+		System.out.println("Hello World!");
 	}
 	
 }
