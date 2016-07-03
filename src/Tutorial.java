b0f4d72d282a4bc79f3786224c8cd0791629a024
bopes <clcblee@gmail.com>
2016-07-03 15:26:46 -0700

    Create Hello World

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -3,7 +3,8 @@ public class Tutorial {
 	
 	public static void main(String[] args){
 //		this is our main void
-		System.out.println("Hello World!");
+		String text = "Hello World!!";
+		System.out.println(text);
 	}
 	
 }
