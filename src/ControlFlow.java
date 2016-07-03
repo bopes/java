6b7d22395ad84c86cdf2bc2ad54b8c606b406366
bopes <clcblee@gmail.com>
2016-07-03 16:08:03 -0700

    Move control flow to new Class

---
+++ src/ControlFlow.java
@@ -0,0 +1,22 @@
+// This is a tutorial
+public class ControlFlow {
+	
+	public static void main(String[] args){
+//		this is our main void
+		int number = 24;
+			
+			if (number == 24) {
+				
+//				Write
+				System.out.println("Number is 24");
+				
+			} 
+				else {
+				
+//					Write
+					System.out.println("Number is not 24");
+				
+				}
+	}
+	
+}
--- src/Tutorial.java
+++ src/Tutorial.java
@@ -1,22 +1,6 @@
-// This is a tutorial
+
 public class Tutorial {
 	
-	public static void main(String[] args){
-//		this is our main void
-		int number = 24;
-			
-			if (number == 24) {
-				
-//				Write
-				System.out.println("Number is 24");
-				
-			} 
-				else {
-				
-//					Write
-					System.out.println("Number is not 24");
-				
-				}
-	}
+
 	
 }
