f30a32130668b24dbaf9a40ba1cb6f3cbf633382
bopes <clcblee@gmail.com>
2016-07-04 13:37:59 -0700

    Create full file writer

--- src/FW.java
+++ src/FW.java
@@ -1,4 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 public class FW {
 
+	public static void main(String[] args) {
+		File newFile = new File("/Users/charleslee/Desktop/Eclipse/Tutorial/newFile.txt");
+		
+		if (newFile.exists()) {
+			System.out.println("File already exists");
+		} else {
+			try {
+				newFile.createNewFile();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				FileWriter fileW = new FileWriter(newFile);
+				BufferedWriter buffW = new BufferedWriter(fileW);
+				buffW.write("It wrote successfully!");
+				buffW.close();
+				System.out.println("File written!");
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
 }
