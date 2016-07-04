ec55b1a636b4f1330443804c9f64b21c439655c9
bopes <clcblee@gmail.com>
2016-07-04 16:40:40 -0700

    Complete try/catch

--- src/TryCatch.java
+++ src/TryCatch.java
@@ -5,16 +5,22 @@ public class TryCatch {
 		
 		// This code may encounter errors
 		// Try lets us run it without errors crashing everything
+		// Only one error will be caught for each try section. If you want to catch multiple errors, Split code into multiple try sections
 		try {
-			int x = 0;
+			// Initial code (Will throw ArithmeticExceptionif x == 0)
+			int x = 1;
 			int y = 2;
 			int answer = y / x;
-			
 			System.out.println(answer);
+			
+			// Another error (Will throw NullPointerException)
+			Object obj = null;
+			obj.hashCode();
 		}
 		
 		// Catch lets us handle any errors arising from code in 'try'
-		catch(ArithmeticException mathError) {
+		// You can specify the type of error/exception to catch with this code block
+		catch (ArithmeticException mathError) {
 			
 			// Store system error message as String
 			String errorMessage = mathError.getMessage();
@@ -29,6 +35,19 @@ public class TryCatch {
 			} else {
 				System.out.println("Math Error");
 			}
+		}
+		// Catching another Exception type in the same try block
+		catch (NullPointerException objError) {
+			String errorMessage = objError.getMessage();
+			System.out.println("Null Pointer Exception: " + errorMessage);
+		}
+		// Catch all Exceptions
+		catch (Exception error) {
+			String errorMessage = error.getMessage();
+			System.out.println("Error: " + errorMessage);
+			
+			// Use #printStackTrace() for debugging
+			error.printStackTrace();
 		}
 		
 	
