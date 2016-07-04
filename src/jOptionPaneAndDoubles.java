0f12e5bf07b106d36da00d4142a53e5bfbf9c586
bopes <clcblee@gmail.com>
2016-07-04 09:25:39 -0700

    Collect info and display via jOptionPane

--- src/jOptionPaneAndDoubles.java
+++ src/jOptionPaneAndDoubles.java
@@ -1,4 +1,12 @@
+import javax.swing.JOptionPane;
 
 public class jOptionPaneAndDoubles {
 
+	public static void main(String[] args){
+		
+		String fingers = JOptionPane.showInputDialog("How many fingers do you have?");
+		JOptionPane.showMessageDialog(null, "You have " + fingers + " fingers.");
+		
+	}
+	
 }
