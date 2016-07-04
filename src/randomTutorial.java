d7fa5e0ef2ee2b9955b0fd9aa485f6ea58bf96a0
bopes <clcblee@gmail.com>
2016-07-03 20:06:29 -0700

    Creat random number generator

--- src/randomTutorial.java
+++ src/randomTutorial.java
@@ -1,6 +1,15 @@
 import java.util.Random;
 public class randomTutorial {
 	
-	
+	public static void main(String[] args){
+		
+		// Instantiate random generator
+		Random rnd = new Random();
+		
+		// Print out random call. Number will be from [0..2). 2 is upper bound, not inclusive
+		System.out.println(rnd.nextInt(2));
+		
+		
+	}
 
 }
