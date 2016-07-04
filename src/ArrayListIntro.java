a9dc2db326001ec56b4f3cc969fdae77c68def97
bopes <clcblee@gmail.com>
2016-07-04 09:41:37 -0700

    Rename class to ArrayListIntro

---
+++ src/ArrayListIntro.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListIntro {
+	
+	static Scanner reader = new Scanner(System.in);
+	static ArrayList<Integer> array = new ArrayList<Integer>();
+	
+	
+
+}
