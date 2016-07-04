9573e07adf0f7a2c0b1a86659478ea11b7f08c51
bopes <clcblee@gmail.com>
2016-07-03 20:20:53 -0700

    Fill in a new array

--- src/Array.java
+++ src/Array.java
@@ -8,7 +8,20 @@ public class Array {
 				
 		// Assigning individual elements in the array
 		arrInt[0] = 1;
-		System.out.println(arrInt[0]);
+		
+		// Assign multiple values (all done individually)
+		int i = 1;
+		while (i <= 4) {
+			arrInt[i] = i + 1;
+			i++;
+		}
+		
+		// View the entire array. If the argument here includes Strings, it will convert the number values to Strings
+		System.out.println(arrInt[0] + " " + arrInt[1] + " " + arrInt[2] + " " + arrInt[3] + " " + arrInt[4]);
+		
+		// Printing the array does not look pretty... this looks like: [I@7852e922
+		System.out.println(arrInt);
+			
 		
 	}
 	
