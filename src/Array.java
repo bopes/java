7a387215c46cb943569964b07d3f19fe1ec0973f
bopes <clcblee@gmail.com>
2016-07-03 20:14:12 -0700

    Create first array

--- src/Array.java
+++ src/Array.java
@@ -1,6 +1,15 @@
 
 public class Array {
 
-	
+	public static void main(String[] args){
+		
+		// initiate array by designating object type it's holding and including '[]' in the variable name. Then specify the length.
+		int arrInt[] = new int[5];
+				
+		// Assigning individual elements in the array
+		arrInt[0] = 1;
+		System.out.println(arrInt[0]);
+		
+	}
 	
 }
