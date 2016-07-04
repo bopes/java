72dccc19d603a2229f17c8f72c7277c78a84d7b2
bopes <clcblee@gmail.com>
2016-07-04 09:46:00 -0700

    Retrieve number array input from user

--- src/ArrayListIntro.java
+++ src/ArrayListIntro.java
@@ -6,6 +6,17 @@ public class ArrayListIntro {
 	static Scanner reader = new Scanner(System.in);
 	static ArrayList<Integer> array = new ArrayList<Integer>();
 	
-	
+	public static void main(String[] args) {
+		System.out.println("Please enter an array of numbers, type 0 when finished");
+		int in = reader.nextInt();
+		while (in != 0) {
+			array.add(in);
+			in = reader.nextInt();
+		}
+		System.out.println("-------------------------------------");
+		for (int i : array) {
+			System.out.println(i);
+		}
+	}
 
 }
