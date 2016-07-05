1888559f18e1ab1a5c679a29c2088112da58b3bc
bopes <clcblee@gmail.com>
2016-07-05 12:02:07 -0700

    Restart - Extend MouseMotionListener

--- src/MouseMotionListenerIntro.java
+++ src/MouseMotionListenerIntro.java
@@ -3,32 +3,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
 
-public class MouseMotionListenerIntro extends Applet {
-	
-	private Graphics globalGraphics;
-	
-	public void paint(Graphics g) {
-		this.setSize(new Dimension(500, 500));
-		globalGraphics = g;
-	}
-	
-	public void drawCircle(int x, int y, int width, int height) {
-		globalGraphics.setColor(getRandomColor());
-		Ellipse2D circle = new Ellipse2D.Double((double)x, (double)y, (double)width, (double)height);
-		Graphics2D g2 = (Graphics2D) globalGraphics;
-		g2.fill(circle);
-	}
-	
-	public Color getRandomColor() {
-		int r = (int) (Math.random() * 256);
-		int g = (int) (Math.random() * 256);
-		int b = (int) (Math.random() * 256);
-		
-		return new Color(r,g,b);
-	}
-	
-	
+public class MouseMotionListenerIntro 
+extends Applet 
+implements MouseMotionListener {
 
+	@Override
+	public void mouseDragged(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+	}
+
+	
+	
 }
