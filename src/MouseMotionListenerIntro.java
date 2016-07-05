c18fd7ee73d51d5176c4b7c58fc088df9a4ff49b
bopes <clcblee@gmail.com>
2016-07-05 12:58:59 -0700

    Add mouse listeners to change draw shape

--- src/MouseMotionListenerIntro.java
+++ src/MouseMotionListenerIntro.java
@@ -4,16 +4,19 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
 
 public class MouseMotionListenerIntro 
 extends Applet 
-implements MouseMotionListener {
+implements MouseMotionListener, MouseListener {
 	
 	private Graphics globalGraphics;
+	private int mouseButton;
 	
 	public void init() {
+		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
 	}
 	
@@ -22,9 +25,14 @@ implements MouseMotionListener {
 		globalGraphics = g.create();
 	}
 	
-	public void drawCircle(int x, int y, int width, int height){
+	public void drawCircle(int x, int y, int width, int height) {
 		globalGraphics.setColor(getColor());
 		globalGraphics.fillOval(x, y, width, height);
+	}
+	
+	public void drawSquare(int x, int y, int width, int height) {
+		globalGraphics.setColor(getColor());
+		globalGraphics.fillRect(x, y, width, height);
 	}
 	
 	public Color getColor(){
@@ -34,20 +42,52 @@ implements MouseMotionListener {
 		return new Color(r,g,b);
 	}
 	
-	public void trackMouse(MouseEvent e, int size){
+	public void mouseAction(MouseEvent e, int size){
 		int mouseX = e.getX();
 		int mouseY = e.getY();
-		drawCircle(mouseX, mouseY, size, size);
+		if (mouseButton == MouseEvent.BUTTON3)
+			drawSquare(mouseX, mouseY, size, size);
+		else
+			drawCircle(mouseX, mouseY, size, size);
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		trackMouse(e, 20);
+		mouseAction(e, 10);
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		trackMouse(e, 10);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		mouseButton = e.getButton();
+		mouseAction(e, 20);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	
