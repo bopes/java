10c433a7bdc0920591f69e29fae07ec11dcb8551
bopes <clcblee@gmail.com>
2016-07-05 19:01:14 -0700

    Create application test thing class

---
+++ src/ApplicationTesterThing.java
@@ -0,0 +1,11 @@
+
+public class ApplicationTesterThing {
+
+	public static void main(String[] args) {
+		System.out.println("1 : Print current year \n"
+						 + "2 : Print programming language\n"
+						 + "3 : Print the first few digits of Pi\n"
+						 + "4 : Print the first few terms of Fibonacci Sequence");
+	}
+	
+}
