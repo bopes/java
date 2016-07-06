f3c67311027aabd5ff7e31760542ad274f13bf89
bopes <clcblee@gmail.com>
2016-07-05 17:43:09 -0700

    Create working switch statement

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -2,6 +2,28 @@
 public class SwitchStatement {
 	
 	public static void main(String[] args) {
+		
+		int day = 2;
+		String dayString = "";
+		
+		switch (day) {
+			case 0: dayString = "Sunday";
+					break;
+			case 1: dayString = "Monday";
+					break;
+			case 2: dayString = "Tuesday";
+					break;
+			case 3: dayString = "Wednesday";
+					break;
+			case 4: dayString = "Thursday";
+					break;
+			case 5: dayString = "Friday";
+					break;
+			case 6: dayString = "Saturday";
+					break;
+		}
+		
+		System.out.println(dayString);
 
 	}
 	
